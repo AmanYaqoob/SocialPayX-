9523b5f37b42be44314d810ae144e07713b46e5b
ismail-qwerty <ismailunnique8@gmail.com>
2026-04-17 15:52:12 +0500

    Add Capacitor Android setup and new APK build

---
+++ frontend/android/app/src/main/java/com/socialpay/x/MainActivity.java
@@ -0,0 +1,5 @@
+package com.socialpay.x;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.socialpay.x',
+  appName: 'SocialPayX',
+  webDir: 'dist'
+};
+
+export default config;
